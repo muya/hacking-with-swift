6fc0710726b0ba0aca5b73561fa04dc8707ee954
Muya <kingkonig@gmail.com>
2015-09-28 08:46:46 +0300

    Complete Storm Viewer App
    Completed first project of the Hacking with Swift Tutorial

---
+++ HWSProject1/HWSProject1/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  HWSProject1
+//
+//  Created by Muya on 24/09/2015.
+//  Copyright © 2015 muya. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailImageView: UIImageView!
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+            
+            self.title = detail
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HWSProject1/HWSProject1/MasterViewController.swift
@@ -0,0 +1,82 @@
+//
+//  MasterViewController.swift
+//  HWSProject1
+//
+//  Created by Muya on 24/09/2015.
+//  Copyright © 2015 muya. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [String]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Storm Viewer"
+        let fm = NSFileManager.defaultManager()
+        let path = NSBundle.mainBundle().resourcePath!
+        let items = try! fm.contentsOfDirectoryAtPath(path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                objects.append(item)
+            }
+        }
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let navigationController = segue.destinationViewController as! UINavigationController
+                let controller = navigationController.topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+
+}
+
