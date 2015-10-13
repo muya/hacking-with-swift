902c4986d40dadd55b52ec5de16cdb41a52d16b4
Muya <kingkonig@gmail.com>
2015-10-13 07:37:04 +0300

    Implement Autolayout using code

--- HWSProject6b/HWSProject6b/ViewController.swift
+++ HWSProject6b/HWSProject6b/ViewController.swift
@@ -13,6 +13,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label1 = createUILabel(UIColor.redColor(), text: "THESE")
+        let label2 = createUILabel(UIColor.cyanColor(), text: "ARE")
+        let label3 = createUILabel(UIColor.yellowColor(), text: "SOME")
+        let label4 = createUILabel(UIColor.greenColor(), text: "AWESOME")
+        let label5 = createUILabel(UIColor.orangeColor(), text: "LABELS")
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        // add constraints
+        let viewsDictionary = [
+            "label1": label1,
+            "label2": label2,
+            "label3": label3,
+            "label4": label4,
+            "label5": label5
+        ]
+        
+        for label in viewsDictionary.keys {
+            view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        }
+        
+        let metrics = ["labelHeight": 88]
+        
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+    }
+    
+    func createUILabel(color: UIColor, text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = color
+        label.text = text
+        return label
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
     override func didReceiveMemoryWarning() {
