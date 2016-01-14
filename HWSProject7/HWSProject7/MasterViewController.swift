46f05f9861731975f04c3e1670b562189ef69b05
Muya <kingkonig@gmail.com>
2016-01-14 07:44:39 +0300

    Use GCD to push loading task to the background

--- HWSProject7/HWSProject7/MasterViewController.swift
+++ HWSProject7/HWSProject7/MasterViewController.swift
@@ -26,15 +26,25 @@ class MasterViewController: UITableViewController {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
-        if let url = NSURL(string: urlString) {
-            if let data = try? NSData(contentsOfURL: url, options: []) {
-                let json = JSON(data: data)
-                
-                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-                    parseJSON(json)
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) { [unowned self] in
+            if let url = NSURL(string: urlString) {
+                if let data = try? NSData(contentsOfURL: url, options: []) {
+                    let json = JSON(data: data)
+                    
+                    if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                        self.parseJSON(json)
+                    } else {
+                        self.showError()
+                    }
+                } else {
+                    self.showError()
                 }
+            } else {
+                self.showError()
             }
         }
+        
+        
     }
     
     func parseJSON(json: JSON) {
@@ -46,7 +56,18 @@ class MasterViewController: UITableViewController {
             objects.append(obj)
         }
         
-        tableView.reloadData()
+        dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    func showError() {
+        dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+            let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the petitions. Please check your connection and try again", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(ac, animated: true, completion: nil)
+        }
     }
 
     override func viewWillAppear(animated: Bool) {
