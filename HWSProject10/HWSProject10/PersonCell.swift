883079c3ab9ded55be7385cee3f42bc7a5b95d2d
Muya <kingkonig@gmail.com>
2016-01-14 08:05:11 +0300

    Set up storyboard for Preoject 10

---
+++ HWSProject10/HWSProject10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  HWSProject10
+//
+//  Created by Muya on 14/01/2016.
+//  Copyright © 2016 muya. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
---
+++ HWSProject10/HWSProject10/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HWSProject10
+//
+//  Created by Muya on 14/01/2016.
+//  Copyright © 2016 muya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
